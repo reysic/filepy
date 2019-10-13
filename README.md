2ea3703977de2d6864046e6a59adcb5224b9d9ef
Jeremy Hicks <jeremy@reysic.com>
2019-10-12 23:01:36 -0500

    Flesh out functionality, update README

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # filepy
 
 [![Codacy Badge](https://api.codacy.com/project/badge/Grade/9d76472fda35475bb517db86983d4482)](https://www.codacy.com/manual/jeremy-hicks/filepy?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=jeremy-hicks/filepy&amp;utm_campaign=Badge_Grade)
+
+Python script that can be used to delete files (and resulting empty directories) in a specifiable directory older than a specifiable number of days:
+
+![Example](https://github.com/jeremy-hicks/filepy/blob/master/docs/images/example.png)
--- filepy.py
+++ filepy.py
@@ -1,17 +1,60 @@
 import sys
 import os
+import time
 
 provided_path = sys.argv[1]
-print("Provided path:", provided_path)
+older_than_days = int(sys.argv[2])
+print(f"Finding files in {provided_path} older than {older_than_days} days")
 
-dir_iterator = os.scandir(provided_path)
-file_paths = [f.path for f in os.scandir(provided_path) if f.is_file()]
-dir_paths = [f.path for f in dir_iterator if f.is_dir()]
+SECONDS_IN_DAY = 86400
+time_now = time.time()
+cutoff_time = time_now - (older_than_days * SECONDS_IN_DAY)
 
-print("\nFiles:")
-print(len(file_paths))
-print(*file_paths, sep="\n")
+file_paths_to_remove = []
+num_files_to_keep = 0
 
-print("\nFolders:")
-print(len(dir_paths))
-print(*dir_paths, sep="\n")
+for (dirpath, dirnames, filenames) in os.walk(provided_path):
+    for f in filenames:
+        file_path = os.path.join(dirpath, f)
+        if os.path.getctime(file_path) < cutoff_time:
+            file_paths_to_remove.append(file_path)
+        else:
+            num_files_to_keep += 1
+
+user_file_remove_res = (
+    input(
+        f"{len(file_paths_to_remove)} files will be removed and {num_files_to_keep} will be kept. Continue? (y/N): "
+    )
+    .lower()
+    .strip()[:1]
+)
+if not user_file_remove_res == "y":
+    sys.exit(1)
+
+num_files_removed = 0
+for file_path in file_paths_to_remove:
+    os.remove(file_path)
+    num_files_removed += 1
+
+print(f"{num_files_removed} files successfully removed ✨")
+
+user_dir_remove_res = input(f"Remove empty directories? (y/N): ").lower().strip()[:1]
+if not user_dir_remove_res == "y":
+    sys.exit(1)
+
+dir_paths_to_remove = []
+
+for (dirpath, dirnames, filenames) in os.walk(provided_path):
+    for d in dirnames:
+        dir_path = os.path.join(dirpath, d)
+        if not os.listdir(dir_path):
+            dir_paths_to_remove.append(dir_path)
+
+num_dirs_removed = 0
+for dir_path in dir_paths_to_remove:
+    os.rmdir(dir_path)
+    num_dirs_removed += 1
+
+print(f"{num_dirs_removed} empty directories successfully removed ✨")
+
+sys.exit(0)
